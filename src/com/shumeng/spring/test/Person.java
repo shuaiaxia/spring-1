c5c758e9f67895d50d116144a19e284f7dfc137e
shuai <shuaiaxia@163.com>
2017-06-12 15:37:18 +0800

    sss

---
+++ src/com/shumeng/spring/test/Person.java
@@ -0,0 +1,26 @@
+package com.shumeng.spring.test;
+
+public class Person {
+	
+	private String name;
+	private Integer age;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public Person() {
+		super();
+	}
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", age=" + age + "]";
+	}
+}
